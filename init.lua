d0a28709f7c3ad807d9d4020b30681f704645184
IIIullaIII <poetafuso34@yahoo.it>
2026-02-08 01:42:09 +0100

    removed some

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 
 local shields_list = {
-    {name = "admin",       descr = S("Supreme Admin Shield"),  prot = 20},
+    {name = "admin",       descr = S("Supreme Admin Shield"),  prot = 200},
     {name = "staff",       descr = S("Official Staff Shield"), prot = 15},
     {name = "contributor", descr = S("Contributor Shield"),    prot = 12},
     {name = "builder",     descr = S("Builder Shield"),        prot = 10},
@@ -9,20 +9,19 @@ local shields_list = {
 for _, s in ipairs(shields_list) do
     armor:register_armor("c_o_shield:shield_" .. s.name, {
         description = s.descr,
-        -- Icona quadrata pulita per l'hotbar
+
         inventory_image = "c_o_shield_inv_" .. s.name .. ".png",
         
-        -- Texture HD che "veste" il modello 3D sul giocatore
+        -- Texture HD 
         texture = "c_o_shield_shield_" .. s.name .. ".png",
-        
-        -- Questa immagine DEVE avere lo scudo piccolo e centrato per non coprire tutto
+       
         preview = "c_o_shield_shield_" .. s.name .. "_preview.png",
         
         groups = {
             armor_shield = 1,
             armor_heal = 0,
             armor_use = 0,
-            -- Fondamentale per evitare che 3D Armor generi icone automatiche sballate
+   
             armor_name = 1,
         },
         armor_groups = {fleshy = s.prot},
